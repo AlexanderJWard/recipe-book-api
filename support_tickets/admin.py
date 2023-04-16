addf78fabec444cc1e785022063efafcb8c5f712
AlexanderJWard <alexjamesward@hotmail.com>
2023-04-16 18:41:53 +0000

    Add django admin filters for support

--- support_tickets/admin.py
+++ support_tickets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Support
 
-# Register your models here.
+
+class SupportTicketAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'owner')
+    list_display = ('id', 'title', 'status', 'owner', 'issue', 'updated_at')
+    search_fields = ['title', 'status']
+
+
+admin.site.register(Support, SupportTicketAdmin)
