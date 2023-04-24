a681914c2ce880ef43f980ef8c60acf1bda64358
AlexanderJWard <alexjamesward@hotmail.com>
2023-04-24 20:11:34 +0000

    Update settings.py with CORS policy

--- recipe_book_api/settings.py
+++ recipe_book_api/settings.py
@@ -124,6 +124,8 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'recipe_book_api.urls'
 
 TEMPLATES = [
